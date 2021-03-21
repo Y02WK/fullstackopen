a0ae487c28f6cfdbd0e51933661042f88a8de544
Y02WK <waikinyong@gmail.com>
2021-03-21 18:16:20 +0800

    Added part 1a

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,63 +1,58 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
-import {useState} from "react";
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.one} exercises={props.ex1} />
+      <Part name={props.two} exercises={props.ex2} />
+      <Part name={props.three} exercises={props.ex3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+  )
+}
 
 const App = () => {
-	const [ counter, setCounter ] = useState(0)
+  const course = "Half stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
 
-	const courses = [
-		{
-			name: 'Half Stack application development',
-			id: 1,
-			parts: [
-				{
-					name: 'Fundamentals of React',
-					exercises: 10,
-					id: 1
-				},
-				{
-					name: 'Using props to pass data',
-					exercises: 7,
-					id: 2
-				},
-				{
-					name: 'State of a component',
-					exercises: 14,
-					id: 3
-				},
-				{
-					name: 'Redux',
-					exercises: 11,
-					id: 4
-				}
-			]
-		},
-		{
-			name: 'Node.js',
-			id: 2,
-			parts: [
-				{
-					name: 'Routing',
-					exercises: 3,
-					id: 1
-				},
-				{
-					name: 'Middlewares',
-					exercises: 7,
-					id: 2
-				}
-			]
-		}
-	]
-
-
-	return (
-		<div>
-			<Content courses={courses} />
-		</div>
-	)
-
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        one={part1}
+        two={part2}
+        three={part3}
+        ex1={exercises1}
+        ex2={exercises2}
+        ex3={exercises3}
+      />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
+  )
 }
 
 export default App;
